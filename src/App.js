5acd845f0f7aa9fbee9a6bbdaa854514178afdc7
Никита <nikitostokarev22@gmail.com>
2020-08-28 08:49:48 +0300

    test ship params

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React,{ useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  // const [starships, setStarships] = useState([]);
+  const [ship, setShip] = useState([]);
 
-  useEffect( async () => {
-    const response = await fetch("http https://swapi.dev/api/starships/9/");
-    const data = await response.json();
-    const [item] = data.results;
-    console.log(item)
-  }, [])
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const response = await fetch("https://swapi.dev/api/films/2/");
+  //     const data = await response.json();
+  //     setStarships(data.starships);
 
+  //   };
+  //   fetchData();
+  // }, []);
+  useEffect(() => {
+    async function fetchShips() {
+      const response = await fetch("http://swapi.dev/api/starships/11/");
+      const data = await response.json();
+      setShip(data);
+    }
+    fetchShips();
+  }, []);
+  console.log(ship);
   return (
     <div className="App">
-     
+      <div className="container">
+        <div className="shipCompare">
+          <div className="shipCard"></div>
+        </div>
+      </div>
     </div>
   );
 }
